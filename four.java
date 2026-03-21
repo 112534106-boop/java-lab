f17189913b9fb4691afcb02cca8708a823c09296
112534106-boop <112534106@kanchiuniv.ac.in>
2026-03-20 22:04:52 -0700

    Add files via upload

---
+++ four.java
@@ -0,0 +1,9 @@
+impact javautil:*;
+class four
+{
+static public void main(String[] args)throws Exception
+Scanner s1=new Scanner(system.in);
+System.out.print("Enter your name");
+String s2=st.nextLine();
+System.out.print("your name is"+s2);
+}}
